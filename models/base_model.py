fadf9a86eb44b73e437a091fcc406e4d26822675
Travis Adamson <tndadamson@gmail.com>
2023-10-05 15:08:20 -0700

    Adding kwargs process

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # base_model.py
 """Defines a new class BaseModel"""
-import uuid
+import models
+from uuid import uuid4
 from datetime import datetime
 
 
@@ -9,25 +10,38 @@ class BaseModel:
     """Creates an new BaseModel"""
 
     def __init__(self, *args, **kwargs):
-        """Initializes a new instance of BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        """Initializes a new instance of BaseModel.
 
-    def __str__(self):
-        """Creates the string representation of the obj"""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                        self.id, self.__dict__)
+        Args:
+            args (any): Not used
+            kwargs (dict): Key/Value pairs to use as attributes
+        """
+        d_t = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key =="created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, d_t)
+                elif key == "__class__":
+                    pass
+                else:
+                    self.__dict__[key] = value
 
     def save(self):
         """Updates most recent change time"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.today()
 
     def to_dict(self):
         """Creates a dictionary representation of the obj"""
         new_dict = self.__dict__.copy()
-        for key, value in new_dict.items():
-            if key == "created_at" or key == "updated_at":
-                new_dict[key] = value.isoformat()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         new_dict["__class__"] = self.__class__.__name__
         return new_dict
+
+    def __str__(self):
+        """Returns the string representation of the obj"""
+        cls = self.__class__.__name__
+        return "[{}] ({}) {}".format(cls, self.id, self.__dict__)
