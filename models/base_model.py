fe5245608bff757d7322aea40161ba2af80e4d93
meisibley <mkong99@yahoo.com>
2023-10-05 13:04:32 -0700

    added test files

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ from datetime import datetime
 class BaseModel:
     """Creates an new BaseModel"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes a new instance of BaseModel"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
