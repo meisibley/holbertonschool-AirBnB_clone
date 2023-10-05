e8a6544d1a89d95e2677e3aae3134ed11b3c1f5b
Travis Adamson <tndadamson@gmail.com>
2023-10-05 09:35:54 -0700

    Added to dict function

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # base_model.py
 """Defines a new class BaseModel"""
-import UUID
+import uuid
 import datetime
 
 class BaseModel:
@@ -9,13 +9,13 @@ class BaseModel:
 
     def __init__(self):
         """Initializes a new instance of BaseModel"""
-        id = str(uuid.uuid4())
-        created_at = datetime.datetime.now()
-        updated_at = datetime.datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Creates the string representation of the obj"""
-        return ("[{}] ({}) {}".format(self.__name__, self.id, self.__dict__))
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         """Updates most recent change time"""
@@ -23,9 +23,8 @@ class BaseModel:
 
     def to_dict(self):
         """Creates a dictionary representation of the obj"""
-        return {
-                "__class__": self.__name__,
-                "id": self.__dict__,
-                "created_at": self.created_at.isoformat(),
-                "updated_at": self.updated_at.isoformat()
-                }
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
