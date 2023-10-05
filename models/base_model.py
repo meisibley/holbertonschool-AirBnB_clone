6ccd2877c1031f661aa6cc6e47f41ef873771776
meisibley <mkong99@yahoo.com>
2023-10-05 12:18:49 -0700

    trying for fun

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,8 @@ class BaseModel:
 
     def __str__(self):
         """Creates the string representation of the obj"""
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                        self.id, self.__dict__)
 
     def save(self):
         """Updates most recent change time"""
