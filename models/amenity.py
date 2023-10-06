d908137932ad33f330cd6ab4a4c98ebc7378c437
meisibley <mkong99@yahoo.com>
2023-10-06 09:51:40 -0700

    9 more classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""class amenity inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+def __init__(self, name=""):
+    """attribute name is public
+
+    arg:
+        name: a string value
+    """
+
+    self.name = name
--- models/user.py
+++ models/user.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""class User inheirits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """handle user's information
+
+    arg:
+        email: user's email address
+        password: email password
+        first_name: user's first name
+        last_name: user's last name
+    """
+
+    def __init__(self, email="", password="", first_name="", last_name=""):
+        """all attributes are public"""
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
